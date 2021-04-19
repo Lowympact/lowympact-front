1253db7a8650b116a248483ccfac6ff9b957df82
corentinbranchereau <branchereaucorentin@gmail.com>
2021-04-19 14:27:18 +0200

    feat: 404 page created

---
+++ src/pages/Error404.js
@@ -0,0 +1,15 @@
+import logo from '../assets/images/logo.svg';
+
+function Error404() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        <h1>Erreur 404</h1>
+        <a href="/">Retourner à l'accueil</a>
+      </header>
+    </div>
+  );
+}
+
+export default Error404;
