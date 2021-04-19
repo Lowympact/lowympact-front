4341828c66cdd07ffcfeb65cadd39e5214e0de7f
corentinbranchereau <branchereaucorentin@gmail.com>
2021-04-19 10:37:58 +0200

    build: PWA deployment

---
+++ build/serviceWorker.js
@@ -0,0 +1,25 @@
+let CACHE_NAME = 'lowympact-cache-v1';
+const urlsToCache = [
+'/',
+'/index.html',
+];
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    );
+  });
+
