b92117cacfcfc74161fa2c3221a1e13341478157
corentinbranchereau <branchereaucorentin@gmail.com>
2021-04-26 12:35:24 +0200

    feat: first navbar version

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+        navbar
+    </div>
+  );
+}
+
+export default Navbar;
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import logo from '../assets/images/logo.svg';
+import Navbar from '../components/Navbar/Navbar';
 
 function Homepage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="logo" alt="logo" />
-        <h1>Lowympact</h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Lowympact</h1>
+
+        </header>
+      </div>
+    </React.Fragment>
   );
 }
 
