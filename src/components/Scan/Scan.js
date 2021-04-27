13100e1c9f3c6b11bb56375341bf469e4e9961cc
Thomas M <gotamt99@gmail.com>
2021-04-27 23:45:30 +0200

    feat : add a close button on video scan

--- src/components/Scan/Scan.js
+++ src/components/Scan/Scan.js
@@ -12,21 +12,35 @@ import {
 } from "@zxing/library";
 
 class Scan extends React.Component {
-  state = { redirect: false, codeContent: undefined };
+  state = { redirect: false, routeRedirect: undefined };
 
   setRedirect = (resultText) => {
     this.setState({
       redirect: true,
-      codeContent: resultText,
+      routeRedirect: "/products/" + resultText,
     });
   };
   renderRedirect = () => {
     if (this.state.redirect) {
-      return <Redirect to={"/products/" + this.state.codeContent} />;
+      return <Redirect to={this.state.routeRedirect} />;
     }
   };
 
   componentDidMount = () => {
+    // Get the modal
+    var video = document.getElementById("video");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = () => {
+      this.setState({
+        redirect: true,
+        routeRedirect: "/history",
+      });
+    };
+
     const hints = new Map();
     const formats = [
       BarcodeFormat.QR_CODE,
@@ -51,9 +65,11 @@ class Scan extends React.Component {
     return (
       <React.Fragment>
         {this.renderRedirect()}
+
         <Header />
         <Navbar />
         <div className="App">
+          <span class="close">&times;</span>
           <video id="video"></video>
         </div>
       </React.Fragment>
