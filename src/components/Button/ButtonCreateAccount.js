cf20fa60ad54caf999f85e3fd9cf7b7f8f11103a
dudacorradini <m.corradini@usp.br>
2021-04-27 08:55:04 +0200

    style: navbar shadow

---
+++ src/components/Button/ButtonCreateAccount.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ButtonCreateAccount() {
+    return (
+      <div>
+        Créer un compte
+      </div>
+    );
+  }
+  export default ButtonCreateAccount;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import logo from '../assets/images/logo.svg';
+import './Login.css';
+import ButtonCreateAccount from '../components/Button/ButtonCreateAccount.js';
 
 class Login extends React.Component{
     render = () =>{
         return (
-            <div>
-                Login
-            </div>
+            <React.Fragment>
+                <div className="App">
+                    <img src={logo} className="logo"/>
+                </div>
+                <div className="login">
+                    Konsume
+                </div>
+                <div className="button-account">
+                    <ButtonCreateAccount />
+                </div>
+            </React.Fragment>
+            
         );
     }
 }
