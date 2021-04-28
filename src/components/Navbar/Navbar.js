5f00808d17901815058a7270f2dd742fa1f8ffd4
corentinbranchereau <branchereaucorentin@gmail.com>
2021-04-28 08:29:05 +0200

    feat: working scanner

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,26 +3,47 @@ import React from "react";
 import ButtonHistory from "../Button/ButtonHistory";
 import ButtonProfil from "../Button/ButtonProfil";
 import ButtonScan from "../Button/ButtonScan";
+import Scan from "../Scan/Scan";
 
-function Navbar() {
-  return (
-    <React.Fragment>
-      <div className="navbar-container">
-        <a href="/history" className="navbar-link navbar-text-left">
-          <ButtonHistory />
-        </a>
-        <a href="/scan">
-          <div className="navbar-circle">
-            <ButtonScan />
-          </div>
-        </a>
-        <span className="navbar-scan-text">scan</span>
-        <a href="/profil" className="navbar-link navbar-text-right">
-          <ButtonProfil />
-        </a>
-      </div>
-    </React.Fragment>
-  );
+class Navbar extends React.Component {
+	state = {
+		showScanner: false,
+	};
+
+	handleScannerButton = (bool) => {
+		this.setState({ showScanner: bool });
+	};
+
+	render = () => {
+		if (this.state.showScanner) {
+			return <Scan onCrossClicked={this.handleScannerButton} />;
+		} else {
+			return (
+				<React.Fragment>
+					<div className="navbar-container">
+						<a
+							href="/history"
+							className="navbar-link navbar-text-left"
+						>
+							<ButtonHistory />
+						</a>
+						<div onClick={() => this.handleScannerButton(true)}>
+							<div className="navbar-circle">
+								<ButtonScan />
+							</div>
+						</div>
+						<span className="navbar-scan-text">scan</span>
+						<a
+							href="/profil"
+							className="navbar-link navbar-text-right"
+						>
+							<ButtonProfil />
+						</a>
+					</div>
+				</React.Fragment>
+			);
+		}
+	};
 }
 
 export default Navbar;
--- src/components/Scan/Scan.js
+++ src/components/Scan/Scan.js
@@ -1,80 +1,86 @@
 import React from "react";
 import "./Scan.css";
-import fruits from "../../assets/images/fruits-vegetables-basket-by-oblik-studio.svg";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import { Redirect } from "react-router-dom";
 import {
-  BarcodeFormat,
-  DecodeHintType,
-  BrowserCodeReader,
-  MultiFormatReader,
+	BarcodeFormat,
+	DecodeHintType,
+	BrowserCodeReader,
+	MultiFormatReader,
 } from "@zxing/library";
 
 class Scan extends React.Component {
-  state = { redirect: false, routeRedirect: undefined };
+	state = { redirect: false, routeRedirect: undefined };
 
-  setRedirect = (resultText) => {
-    this.setState({
-      redirect: true,
-      routeRedirect: "/products/" + resultText,
-    });
-  };
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to={this.state.routeRedirect} />;
-    }
-  };
+	setRedirect = (resultText) => {
+		this.setState({
+			redirect: true,
+			routeRedirect: "/products/" + resultText,
+		});
+	};
 
-  componentDidMount = () => {
-    // Get the modal
-    var video = document.getElementById("video");
+	renderRedirect = () => {
+		if (this.state.redirect) {
+			return <Redirect to={this.state.routeRedirect} />;
+		}
+	};
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+	componentDidMount = () => {
+		// 	// Get the modal
+		// 	// var video = document.getElementById("video");
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = () => {
-      this.setState({
-        redirect: true,
-        routeRedirect: "/history",
-      });
-    };
+		// 	// Get the <span> element that closes the modal
+		// 	var span = document.getElementsByClassName("close")[0];
 
-    const hints = new Map();
-    const formats = [
-      BarcodeFormat.QR_CODE,
-      BarcodeFormat.DATA_MATRIX /*, ...*/,
-    ];
+		// 	// When the user clicks on <span> (x), close the modal
+		// 	span.onclick = () => {
+		// 		this.setState({
+		// 			redirect: true,
+		// 			routeRedirect: "/history",
+		// 		});
+		// 	};
 
-    hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
+		const hints = new Map();
+		const formats = [
+			BarcodeFormat.QR_CODE,
+			BarcodeFormat.EAN_13,
+			BarcodeFormat.DATA_MATRIX /*, ...*/,
+		];
 
-    let codeReaderFormat = new MultiFormatReader();
-    codeReaderFormat.setHints(hints);
+		hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
-    let multiFormatReader = new BrowserCodeReader(codeReaderFormat);
-    multiFormatReader
-      .decodeFromInputVideoDevice(undefined, "video")
-      .then((result) => {
-        this.setRedirect(result.text);
-      })
-      .catch((err) => console.error(err));
-  };
+		let codeReaderFormat = new MultiFormatReader();
+		codeReaderFormat.setHints(hints);
 
-  render = () => {
-    return (
-      <React.Fragment>
-        {this.renderRedirect()}
+		let multiFormatReader = new BrowserCodeReader(codeReaderFormat);
+		multiFormatReader
+			.decodeFromInputVideoDevice(undefined, "video")
+			.then((result) => {
+				this.setRedirect(result.text);
+			})
+			.catch((err) => console.error(err));
+	};
 
-        <Header />
-        <Navbar />
-        <div className="App">
-          <span class="close">&times;</span>
-          <video id="video"></video>
-        </div>
-      </React.Fragment>
-    );
-  };
+	render = () => {
+		return (
+			<React.Fragment>
+				{this.renderRedirect()}
+
+				<Header />
+				<Navbar />
+				<div className="App">
+					<span
+						class="close"
+						onClick={() => this.props.onCrossClicked(false)}
+					>
+						&times;
+					</span>
+					<video id="video"></video>
+				</div>
+			</React.Fragment>
+		);
+	};
 }
 
 export default Scan;
