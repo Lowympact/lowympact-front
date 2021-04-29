7526f4d3274bd314ad0f8402747728354c26fe6a
Thomas M <gotamt99@gmail.com>
2021-04-29 18:21:33 +0200

    fix : first functionnal implementation with yuka scanner

--- src/components/Scan/Scan.js
+++ src/components/Scan/Scan.js
@@ -1,102 +1,114 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  configure,
+  BarcodePicker as ScanditSDKBarcodePicker,
+} from "scandit-sdk";
 import "./Scan.css";
-// import "./Toogle.css";
-import QrReader from "react-qr-reader";
-import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 
-import * as ScanditSDK from "scandit-sdk";
+// Configure the library and activate it with a license key
+const configurationPromise = configure(
+  "ASHf6Cx7DGz5BbkR5RWee94Ezm55DtkCAFB5HoNVQLhPf8gUhk8ypmZ72tbfAdIo33/I2gZR9pcXcdBwQGyYu69PkahqLA0dEEN5WFtpVEjMfQMiBhwmRWIJQgn+E54DiwJm/5CjsV1+Uy7AJWXnvrLiuwxG2D/b7b1QoPFJfkKp2OSreWJXnV/kJ+rtqFyNOu2wQb5Rq5IFXOU/tnjlTvJp8Kxl4jezOVoWlQZreaLKfxfBforPArJErC16cldrqFULYUdsPqb4Vn27hWUmPQTfaX/SbeDCl1h++IPmcvRpzwM9hY0ti7zzfhBKg3TgEYTMiPUipakmM21mU92xYjA9kmKO6bOjzY4XqyrWBUzv/C1TwJfK1aZDDLH8BOmqqJILi2Ty9fN84y09vBgv0dfXhQa3Q0/UW0Hnv0Xge2am3RY5dmE6pfWaxYyA3fDj920DpnlUNcAsfdErnvpZz8H1eftw6D2w3ps6q3jNFc/CTkALFZOk9Sy2u+lOfzvjKrcz5d1yoSo1sVhFsLWNUoaoHTP6NPnQaawC1YwPRp90T+2yNfx+iESAhn3qfn2pOEpBG0rHdZNLLj1yeNJU9Pjt7dznNeCJwG2SzSQvPZU5HzLuIu+siWjUPStB9WWMfVluWN3opv+ReRWBjBsaRFPBmit6LNbRk3QLT6To50yVZg8Zy1NuDFUoE+Ms2ytVn9fs30n1DeNYJLCWjQJ4ktQaz2mIFTOFvRVrJqPkTBwzOaAjWoPxOr097hga8bRhPAqV6ir7ojSffUeCjL/jmp1x1uSE4OaQe3Y32oC0XOVJqA==",
+  {
+    engineLocation: "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build/",
+  }
+).catch((error) => {
+  alert(error);
+});
 
-class Scan extends React.Component {
-	state = {
-		barcode: undefined,
-		bcProductId: undefined,
-	};
+const style = {
+  position: "absolute",
+  top: "0",
+  bottom: "0",
+  left: "0",
+  right: "0",
+  margin: "auto",
+  maxWidth: "1280px",
+  maxHeight: "80%",
+};
 
-	componentDidMount = () => {
-		// inside an async function
-		ScanditSDK.configure(
-			"ASHf6Cx7DGz5BbkR5RWee94Ezm55DtkCAFB5HoNVQLhPf8gUhk8ypmZ72tbfAdIo33/I2gZR9pcXcdBwQGyYu69PkahqLA0dEEN5WFtpVEjMfQMiBhwmRWIJQgn+E54DiwJm/5CjsV1+Uy7AJWXnvrLiuwxG2D/b7b1QoPFJfkKp2OSreWJXnV/kJ+rtqFyNOu2wQb5Rq5IFXOU/tnjlTvJp8Kxl4jezOVoWlQZreaLKfxfBforPArJErC16cldrqFULYUdsPqb4Vn27hWUmPQTfaX/SbeDCl1h++IPmcvRpzwM9hY0ti7zzfhBKg3TgEYTMiPUipakmM21mU92xYjA9kmKO6bOjzY4XqyrWBUzv/C1TwJfK1aZDDLH8BOmqqJILi2Ty9fN84y09vBgv0dfXhQa3Q0/UW0Hnv0Xge2am3RY5dmE6pfWaxYyA3fDj920DpnlUNcAsfdErnvpZz8H1eftw6D2w3ps6q3jNFc/CTkALFZOk9Sy2u+lOfzvjKrcz5d1yoSo1sVhFsLWNUoaoHTP6NPnQaawC1YwPRp90T+2yNfx+iESAhn3qfn2pOEpBG0rHdZNLLj1yeNJU9Pjt7dznNeCJwG2SzSQvPZU5HzLuIu+siWjUPStB9WWMfVluWN3opv+ReRWBjBsaRFPBmit6LNbRk3QLT6To50yVZg8Zy1NuDFUoE+Ms2ytVn9fs30n1DeNYJLCWjQJ4ktQaz2mIFTOFvRVrJqPkTBwzOaAjWoPxOr097hga8bRhPAqV6ir7ojSffUeCjL/jmp1x1uSE4OaQe3Y32oC0XOVJqA==",
-			{
-				engineLocation:
-					"https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build/",
-			}
-		)
-			.then(() => {
-				return ScanditSDK.BarcodePicker.create(
-					document.getElementById("scandit-barcode-picker"),
-					{
-						// enable some common symbologies
-						scanSettings: new ScanditSDK.ScanSettings({
-							enabledSymbologies: [
-								"ean8",
-								"ean13",
-								"upca",
-								"upce",
-								"QR",
-							],
-						}),
-					}
-				);
-			})
-			.then((barcodePicker) => {
-				// barcodePicker is ready here, show a message every time a barcode is scanned
-				barcodePicker.on("scan", (scanResult) => {
-					alert(scanResult.barcodes[0].data);
-				});
-			});
-	};
-
-	// check compatibility
-
-	handleBarcode = (err, res) => {
-		if (res && res.text) {
-			console.log("barcode", res.text);
-			//détecté par barcode scanner
-			let arr = res.text.split("/");
-			console.log(arr);
-
-			this.setState({ barcode: arr[0] });
-
-			if (arr.length === 2) {
-				this.setState({ bcProductId: arr[1] });
-			}
-		}
-	};
-
-	/*
-  setModeScan = () => {
-    this.setState({ QRcodeScan: !this.state.QRcodeScan });
+class Scan extends Component {
+  static propTypes = {
+    visible: PropTypes.bool,
+    playSoundOnScan: PropTypes.bool,
+    vibrateOnScan: PropTypes.bool,
+    scanningPaused: PropTypes.bool,
+    guiStyle: PropTypes.string,
+    videoFit: PropTypes.string,
+    scanSettings: PropTypes.object,
+    enableCameraSwitcher: PropTypes.bool,
+    enableTorchToggle: PropTypes.bool,
+    enableTapToFocus: PropTypes.bool,
+    enablePinchToZoom: PropTypes.bool,
+    accessCamera: PropTypes.bool,
+    camera: PropTypes.object,
+    cameraSettings: PropTypes.object,
+    targetScanningFPS: PropTypes.number,
+    onScan: PropTypes.func,
+    onError: PropTypes.func,
   };
-  */
 
-	render = () => {
-		/*if (this.state.QRcodeScan) {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    configurationPromise.then(() => {
+      ScanditSDKBarcodePicker.create(this.ref.current, this.props).then(
+        (barcodePicker) => {
+          this.barcodePicker = barcodePicker;
+
+          if (this.props.onScan != null) {
+            barcodePicker.on("scan", this.props.onScan);
+          }
+          if (this.props.onError != null) {
+            barcodePicker.on("scanError", this.props.onError);
+          }
+        }
+      );
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.barcodePicker != null) {
+      this.barcodePicker.destroy();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    // These are just some examples of how to react to some possible property changes
+
+    if (
+      JSON.stringify(prevProps.scanSettings) !==
+      JSON.stringify(this.props.scanSettings)
+    ) {
+      this.barcodePicker.applyScanSettings(this.props.scanSettings);
+    }
+
+    if (prevProps.visible !== this.props.visible) {
+      this.barcodePicker.setVisible(this.props.visible);
+    }
+  }
+
+  render() {
     return (
       <React.Fragment>
-        <span class="close" onClick={() => this.props.onCrossClicked(false)}>
-          &times;
-        </span>
-        <QrReader delay={1} onScan={this.handleScan} className="qrcode" />
-
-        <div className="barcode-popup">{this.state.barcode}</div>
+        <div id="background-scan">
+          <div ref={this.ref} style={style} />
+          <span
+            class="close"
+            onClick={() => {
+              this.props.onCrossClicked(false);
+            }}
+          >
+            &times;
+          </span>
+          <div id="scandit-barcode-picker"></div>
+        </div>
       </React.Fragment>
     );
-    } else {
-		*/
-		return (
-			<React.Fragment>
-				<div id="background-scan">
-					<span
-						class="close"
-						onClick={() => this.props.onCrossClicked(false)}
-					>
-						&times;
-					</span>
-					<div id="scandit-barcode-picker"></div>
-				</div>
-			</React.Fragment>
-		);
-	};
+  }
 }
 
 export default Scan;
