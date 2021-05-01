35b573f8b489164f51f4b931972802825644ee1b
corentinbranchereau <branchereaucorentin@gmail.com>
2021-05-01 20:18:42 +0200

    feat: remove useless import

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -11,31 +11,31 @@ import "./Product.css";
 // import Box from "@material-ui/core/Box";
 // import PropTypes from "prop-types";
 
-function TabPanel(props) {
-	const { children, value, index, ...other } = props;
+// function TabPanel(props) {
+// 	const { children, value, index, ...other } = props;
 
-	return (
-		<div
-			role="tabpanel"
-			hidden={value !== index}
-			id={`simple-tabpanel-${index}`}
-			aria-labelledby={`simple-tab-${index}`}
-			{...other}
-		>
-			{value === index && (
-				<Box p={3}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	);
-}
+// 	return (
+// 		<div
+// 			role="tabpanel"
+// 			hidden={value !== index}
+// 			id={`simple-tabpanel-${index}`}
+// 			aria-labelledby={`simple-tab-${index}`}
+// 			{...other}
+// 		>
+// 			{value === index && (
+// 				<Box p={3}>
+// 					<Typography>{children}</Typography>
+// 				</Box>
+// 			)}
+// 		</div>
+// 	);
+// }
 
-TabPanel.propTypes = {
-	children: PropTypes.node,
-	index: PropTypes.any.isRequired,
-	value: PropTypes.any.isRequired,
-};
+// TabPanel.propTypes = {
+// 	children: PropTypes.node,
+// 	index: PropTypes.any.isRequired,
+// 	value: PropTypes.any.isRequired,
+// };
 
 class Product extends React.Component {
 	state = {
