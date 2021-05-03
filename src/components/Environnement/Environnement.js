e3639e657f1f7b01cc2fe4df6bd46770e985f8a9
Thomas M <gotamt99@gmail.com>
2021-05-03 23:11:10 +0200

    feat : display transport impact with an average method

--- src/components/Environnement/Environnement.js
+++ src/components/Environnement/Environnement.js
@@ -3,194 +3,257 @@ import "./Environnement.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/swiper-bundle.css";
+import { CircleProgress } from "react-gradient-progress";
 
 class Environnement extends React.Component {
-	state = {
-		swiper: undefined,
-		currentIndex: 0,
-	};
+  state = {
+    swiper: undefined,
+    currentIndex: 0,
+    width: undefined,
+  };
 
-	getMaterialIcon = (mode) => {
-		switch (mode) {
-			case "Recyclable":
-				return "check_circle_outline";
-			case "Non recyclable":
-				return "highlight_off";
-			default:
-				return "view_in_ar";
-		}
-	};
+  getMaterialIcon = (mode) => {
+    switch (mode) {
+      case "Recyclable":
+        return "check_circle_outline";
+      case "Non recyclable":
+        return "highlight_off";
+      case "Truck":
+        return "local_shipping";
+      default:
+        return "view_in_ar";
+    }
+  };
 
-	getColor = (note) => {
-		if (note > 67) {
-			return "green";
-		}
-		if (note <= 33) {
-			return "red";
-		}
-		return "yellow";
-	};
+  getColor = (note) => {
+    if (note > 67) {
+      return "green";
+    }
+    if (note <= 33) {
+      return "red";
+    }
+    return "yellow";
+  };
 
-	getLabel = (note) => {
-		if (note > 67) {
-			return "Bonne";
-		}
-		if (note <= 33) {
-			return "Mauvais";
-		}
-		return "Moyen";
-	};
+  getColorImpact = (note) => {
+    if (note > 70) {
+      return "green";
+    }
+    if (note <= 40) {
+      return "red";
+    }
+    return "yellow";
+  };
 
-	displaySlides = () => {
-		let slides = <React.Fragment></React.Fragment>;
+  getLabel = (note) => {
+    if (note > 67) {
+      return "Bonne";
+    }
+    if (note <= 33) {
+      return "Mauvais";
+    }
+    return "Moyen";
+  };
 
-		if (
-			this.props.dataEcoScore &&
-			this.props.dataEcoScore?.adjustments?.packaging?.packagings
-				.length >= 1
-		) {
-			slides = this.props.dataEcoScore?.adjustments?.packaging?.packagings.map(
-				(data) => {
-					var recyclable = "";
-					if (data.recycling) {
-						recyclable = data.recycling.split(":")[1];
-					}
-					if (recyclable && recyclable == "recycle") {
-						recyclable = "Recyclable";
-					} else if (recyclable && recyclable == "discard") {
-						recyclable = "Non recyclable";
-					} else {
-						recyclable = "";
-					}
+  getLabelImpact = (note) => {
+    if (note > 60) {
+      return "Impact fort";
+    }
+    if (note <= 30) {
+      return "Impact faible";
+    }
+    return "Impact moyen";
+  };
 
-					return (
-						<SwiperSlide>
-							<div className="env-product-slide-container ">
-								<div className="env-product-slide-icon">
-									<span class="material-icons env-icon-label">
-										{this.getMaterialIcon("")}
-									</span>
-								</div>
-								<div className="env-product-slide-wrapper">
-									<div className="product-slide-name">
-										{data?.material.split(":")[1]}
-									</div>
-									<div className="product-ecoscore">
-										Ecoscore :{" "}
-										{data?.ecoscore_material_score}
-									</div>
+  displaySlides = () => {
+    let slides = <React.Fragment></React.Fragment>;
 
-									<div className="env-history-label-container">
-										<span
-											className="packaging-label-color"
-											style={{
-												color: this.getColor(
-													data?.ecoscore_material_score
-												),
-											}}
-										>
-											●
-										</span>
-										<div className="env-history-label">
-											{this.getLabel(
-												data?.ecoscore_material_score
-											)}
-										</div>
-									</div>
-									<div className="product-slide-recyclable">
-										{recyclable}
-										<div className="material-icons icon-label-recyclable">
-											{this.getMaterialIcon(recyclable)}
-										</div>
-									</div>
-								</div>
-							</div>
-						</SwiperSlide>
-					);
-				}
-			);
-		}
-		return slides;
+    if (
+      this.props.dataEcoScore &&
+      this.props.dataEcoScore?.adjustments?.packaging?.packagings.length >= 1
+    ) {
+      slides = this.props.dataEcoScore?.adjustments?.packaging?.packagings.map(
+        (data) => {
+          var recyclable = "";
+          if (data.recycling) {
+            recyclable = data.recycling.split(":")[1];
+          }
+          if (recyclable && recyclable == "recycle") {
+            recyclable = "Recyclable";
+          } else if (recyclable && recyclable == "discard") {
+            recyclable = "Non recyclable";
+          } else {
+            recyclable = "";
+          }
 
-		// return slides;
-	};
+          return (
+            <SwiperSlide>
+              <div className="env-product-slide-container ">
+                <div className="env-product-slide-icon">
+                  <span class="material-icons env-icon-label">
+                    {this.getMaterialIcon("")}
+                  </span>
+                </div>
+                <div className="env-product-slide-wrapper">
+                  <div className="product-slide-name">
+                    {data?.material.split(":")[1]}
+                  </div>
+                  <div className="product-ecoscore">
+                    Ecoscore : {data?.ecoscore_material_score}
+                  </div>
 
-	displayPackaging = () => {
-		let res = (
-			<React.Fragment>
-				<p>Matériaux utilisés</p>
-			</React.Fragment>
-		);
-		//if (this.props.dataEcoScore.adjustments.packaging) {
-		res = this.props.dataEcoScore?.adjustments?.packaging?.packagings.map(
-			(product) => {
-				console.log(product);
-				return (
-					<div>
-						<p>Matériel : {product?.material}</p>
-					</div>
-				);
-			}
-		);
-		//}
-		return res;
-	};
+                  <div className="env-history-label-container">
+                    <span
+                      className="packaging-label-color"
+                      style={{
+                        color: this.getColor(data?.ecoscore_material_score),
+                      }}
+                    >
+                      ●
+                    </span>
+                    <div className="env-history-label">
+                      {this.getLabel(data?.ecoscore_material_score)}
+                    </div>
+                  </div>
+                  <div className="product-slide-recyclable">
+                    {recyclable}
+                    <div className="material-icons icon-label-recyclable">
+                      {this.getMaterialIcon(recyclable)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        }
+      );
+    }
+    return slides;
 
-	onSlideChange = (index) => {
-		this.setState({ currentIndex: index });
-	};
+    // return slides;
+  };
 
-	render = () => {
-		return (
-			<React.Fragment>
-				<span className="title-part-environnement">
-					Impact de l'emballage
-				</span>
-				<Swiper
-					spaceBetween={10}
-					slidesPerView={1}
-					centeredSlides={true}
-					onSlideChange={(i) => this.onSlideChange(i.activeIndex)}
-					onSwiper={(swiper) => this.setState({ swiper: swiper })}
-				>
-					{this.displaySlides()}
-				</Swiper>
-				{/* <Swiper
-					spaceBetween={50}
-					slidesPerView={1}
-					onSlideChange={() => console.log("slide change")}
-					onSwiper={(swiper) => console.log(swiper)}
-				>
-					{this.displaySlides()}
-					<SwiperSlide>
-						<div className="test">Slide 1</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div className="test">Slide 2</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div className="test">Slide 3</div>
-					</SwiperSlide>
-				</Swiper> */}
-			</React.Fragment>
-		);
-	};
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  displayTransportImpact = () => {
+    let res = <React.Fragment></React.Fragment>;
+
+    var agribalyse_CO2 = undefined;
+    var transportation_score = undefined;
+    var transport_final_indicator = undefined;
+    var n = 0;
+
+    if (this.props.dataEcoScore?.agribalyse?.co2_transportation) {
+      agribalyse_CO2 =
+        0.5 * this.props.dataEcoScore?.agribalyse?.co2_transportation;
+      n += 1;
+    }
+
+    if (
+      this.props.dataEcoScore?.adjustments?.origins_of_ingredients?.value_fr
+    ) {
+      transportation_score =
+        0.5 +
+        this.props.dataEcoScore?.adjustments?.origins_of_ingredients?.value_fr *
+          (-1.0 / 40.0);
+      n += 1;
+    }
+    if (n > 0) {
+      if (!agribalyse_CO2) {
+        agribalyse_CO2 = 0;
+      }
+      if (!transportation_score) {
+        transportation_score = 0;
+      }
+      transport_final_indicator = (agribalyse_CO2 + transportation_score) / n;
+      if (transport_final_indicator > 1) {
+        transport_final_indicator = 1;
+      }
+      if (transport_final_indicator < 0) {
+        transport_final_indicator = 0;
+      }
+    }
+
+    if (transport_final_indicator) {
+      transport_final_indicator = Math.round(transport_final_indicator * 100);
+      return (
+        <div className="product-transport-impact-container">
+          <div className="product-transport-impact-header">
+            <div className="product-transport-impact-logo">
+              <div className="material-icons icon-label-transport-impact">
+                {this.getMaterialIcon("Truck")}
+              </div>
+            </div>
+            <div className="product-transport-impact-title">
+              <div className="product-transport-impact-title-text">
+                Impact du transport
+              </div>
+              <div className="product-transport-impact-title-label">
+                {this.getLabelImpact(transport_final_indicator)}
+              </div>
+            </div>
+            <div
+              className="product-transport-impact-color-label"
+              style={{
+                color: this.getColorImpact(100 - transport_final_indicator),
+              }}
+            >
+              ●
+            </div>
+          </div>
+          <div className="product-transport-impact-content">
+            <div className="product-transport-impact-content-text">
+              Impact en % :
+            </div>
+            <div className="product-transport-impact-content-progress">
+              <CircleProgress
+                percentage={transport_final_indicator}
+                strokeWidth={window.innerWidth * (1.0 / 50.0)}
+                width={window.innerWidth * (1.0 / 4.0)}
+                fontSize={window.innerWidth * (1.0 / 20.0)}
+                primaryColor={["#FF3333", "#33FF63"]}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    return res;
+  };
+
+  onSlideChange = (index) => {
+    this.setState({ currentIndex: index });
+  };
+
+  render = () => {
+    return (
+      <React.Fragment>
+        <span className="title-part-environnement">Impact de l'emballage</span>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          centeredSlides={true}
+          onSlideChange={(i) => this.onSlideChange(i.activeIndex)}
+          onSwiper={(swiper) => this.setState({ swiper: swiper })}
+        >
+          {this.displaySlides()}
+        </Swiper>
+        {this.displayTransportImpact()}
+      </React.Fragment>
+    );
+  };
 }
-/*
-   <div>
-          <p>
-            Note du transport des ingrédients :
-            {
-              this.props.dataEcoScore?.adjustments?.origins_of_ingredients
-                ?.transportation_value_fr
-            }
-          </p>
-          <p>Note de l'éco score : {this.props.dataEcoScore?.score} </p>
-        </div>
-        <div>
-          <p>Informations emballages </p>
-          {this.displayPackaging()}
-        </div>
-*/
 
 export default Environnement;
