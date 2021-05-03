bae47f4cd9ce73eb3a2c107662e403d4f6f25f45
Thomas M <gotamt99@gmail.com>
2021-05-03 08:18:58 +0200

    style : display packaging informations

--- src/components/Environnement/Environnement.js
+++ src/components/Environnement/Environnement.js
@@ -1,7 +1,118 @@
 import React from "react";
 import "./Environnement.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/swiper-bundle.css";
 
 class Environnement extends React.Component {
+  state = {
+    swiper: undefined,
+    currentIndex: 0,
+  };
+
+  getMaterialIcon = (mode) => {
+    switch (mode) {
+      case "Recyclable":
+        return "check_circle_outline";
+      case "Non recyclable":
+        return "highlight_off";
+      default:
+        return "view_in_ar";
+    }
+  };
+
+  getColor = (note) => {
+    if (note > 67) {
+      return "green";
+    }
+    if (note <= 33) {
+      return "red";
+    }
+    return "yellow";
+  };
+
+  getLabel = (note) => {
+    if (note > 67) {
+      return "Bonne";
+    }
+    if (note <= 33) {
+      return "Mauvais";
+    }
+    return "Moyen";
+  };
+
+  displaySlides = () => {
+    let slides = <React.Fragment></React.Fragment>;
+
+    if (
+      this.props.dataEcoScore &&
+      this.props.dataEcoScore?.adjustments?.packaging?.packagings.length >= 1
+    ) {
+      slides = this.props.dataEcoScore?.adjustments?.packaging?.packagings.map(
+        (data) => {
+          console.log(data);
+          var recyclable = "";
+          if (data.recycling) {
+            recyclable = data.recycling.split(":")[1];
+          }
+
+          if (recyclable && recyclable == "recycle") {
+            recyclable = "Recyclable";
+          } else if (recyclable && recyclable == "discard") {
+            recyclable = "Non recyclable";
+          } else {
+            recyclable = "?";
+          }
+
+          return (
+            <React.Fragment>
+              <SwiperSlide>
+                <div className="env-product-slide-container ">
+                  <div className="env-product-slide-icon">
+                    <span class="material-icons env-icon-label">
+                      {this.getMaterialIcon("")}
+                    </span>
+                  </div>
+                  <div className="env-product-slide-wrapper">
+                    <div className="product-slide-name">
+                      {data?.material.split(":")[1]}
+                    </div>
+                    <div className="product-ecoscore">
+                      Ecoscore : {data?.ecoscore_material_score}
+                    </div>
+
+                    <div className="history-label-container">
+                      <span
+                        className="packaging-label-color"
+                        style={{
+                          color: this.getColor(data?.ecoscore_material_score),
+                        }}
+                      >
+                        ●
+                      </span>
+                      <div className=" history-label">
+                        {this.getLabel(data?.ecoscore_material_score)}
+                      </div>
+                    </div>
+                    <div className="product-slide-recyclable">
+                      {recyclable}
+                      <div className="material-icons icon-label-recyclable">
+                        {this.getMaterialIcon(recyclable)}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </React.Fragment>
+          );
+        }
+      );
+    }
+    return slides;
+
+    // return slides;
+  };
+
   displayPackaging = () => {
     let res = (
       <React.Fragment>
@@ -23,10 +134,29 @@ class Environnement extends React.Component {
     return res;
   };
 
+  onSlideChange = (index) => {
+    this.setState({ currentIndex: index });
+  };
+
   render = () => {
     return (
       <React.Fragment>
-        <div>
+        <span className="title-part-environnement">Impact de l'emballage</span>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1.2}
+          centeredSlides={true}
+          onSlideChange={(i) => this.onSlideChange(i.activeIndex)}
+          onSwiper={(swiper) => this.setState({ swiper: swiper })}
+        >
+          {this.displaySlides()}
+        </Swiper>
+      </React.Fragment>
+    );
+  };
+}
+/*
+   <div>
           <p>
             Note du transport des ingrédients :
             {
@@ -36,14 +166,10 @@ class Environnement extends React.Component {
           </p>
           <p>Note de l'éco score : {this.props.dataEcoScore?.score} </p>
         </div>
-
         <div>
           <p>Informations emballages </p>
           {this.displayPackaging()}
         </div>
-      </React.Fragment>
-    );
-  };
-}
+*/
 
 export default Environnement;
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -38,215 +38,207 @@ import { Link } from "react-router-dom";
 // };
 
 class Product extends React.Component {
-	state = {
-		barcode: this.props.match.params.barcode,
-		bcProductId: this.props.match.params.bcProductId,
-		product: undefined,
-		productImageUrl: undefined,
-		productName: undefined,
-		genericName: undefined,
-		ecoScore: undefined,
-		dataEcoScore: undefined,
-		value: 0,
-	};
+  state = {
+    barcode: this.props.match.params.barcode,
+    bcProductId: this.props.match.params.bcProductId,
+    product: undefined,
+    productImageUrl: undefined,
+    productName: undefined,
+    genericName: undefined,
+    ecoScore: undefined,
+    dataEcoScore: undefined,
+    value: 0,
+  };
 
-	handleBarCodeUpdate = () => {
-		if (this.state.barcode !== this.props.match.params.barcode) {
-			this.setState({
-				barcode: this.props.match.params.barcode,
-				bcProductId: this.props.match.params.bcProductId,
-			});
-			this.loadFromOpenFoodFacts(this.props.match.params.barcode);
-			if (this.props.match.params.bcProductId) {
-				this.loadProductInformations(
-					this.props.match.params.barcode,
-					this.props.match.params.bcProductId
-				);
-			}
-		}
-	};
+  handleBarCodeUpdate = () => {
+    if (this.state.barcode !== this.props.match.params.barcode) {
+      this.setState({
+        barcode: this.props.match.params.barcode,
+        bcProductId: this.props.match.params.bcProductId,
+      });
+      this.loadFromOpenFoodFacts(this.props.match.params.barcode);
+      if (this.props.match.params.bcProductId) {
+        this.loadProductInformations(
+          this.props.match.params.barcode,
+          this.props.match.params.bcProductId
+        );
+      }
+    }
+  };
 
-	componentDidMount = () => {
-		this.loadFromOpenFoodFacts(this.props.match.params.barcode);
-		if (this.props.match.params.bcProductId) {
-			this.loadProductInformations(
-				this.props.match.params.barcode,
-				this.props.match.params.bcProductId
-			);
-		}
-	};
-	loadProductInformations = (barcode, bcProductId) => {
-		fetch(
-			`https://api.lowympact.fr/api/v1/products/${barcode}?bcProductId=${bcProductId}`,
-			// `http://localhost:8080/api/v1/products/${this.state.barcode}?bcProductId=${this.state.bcProductId}`,
-			{
-				method: "get",
-				credentials: "include",
-				headers: new Headers({
-					Authorization:
-						"Bearer 99d8fb95-abdd-4885-bf6c-3a81d8874043",
-					"Content-Type": "application/json",
-				}),
-			}
-		)
-			.then((response) => response.json())
-			.then((res) => {
-				console.log(res);
-				this.setState({
-					products: res.data.traceability,
-					impact: res.data.impact,
-				});
-			});
-	};
+  componentDidMount = () => {
+    this.loadFromOpenFoodFacts(this.props.match.params.barcode);
+    if (this.props.match.params.bcProductId) {
+      this.loadProductInformations(
+        this.props.match.params.barcode,
+        this.props.match.params.bcProductId
+      );
+    }
+  };
+  loadProductInformations = (barcode, bcProductId) => {
+    fetch(
+      `https://api.lowympact.fr/api/v1/products/${barcode}?bcProductId=${bcProductId}`,
+      // `http://localhost:8080/api/v1/products/${this.state.barcode}?bcProductId=${this.state.bcProductId}`,
+      {
+        method: "get",
+        credentials: "include",
+        headers: new Headers({
+          Authorization: "Bearer 99d8fb95-abdd-4885-bf6c-3a81d8874043",
+          "Content-Type": "application/json",
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          products: res.data.traceability,
+          impact: res.data.impact,
+        });
+      });
+  };
 
-	loadFromOpenFoodFacts = (barcode) => {
-		fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json/`)
-			.then((response) => response.json())
-			.then((res) => {
-				console.log(res);
-				let productImageUrl = res?.product?.image_url;
-				let productName = res?.product?.product_name;
-				let genericName = res?.product?.generic_name;
-				let ecoScore = res?.product?.ecoscore_grade;
+  loadFromOpenFoodFacts = (barcode) => {
+    fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json/`)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        let productImageUrl = res?.product?.image_url;
+        let productName = res?.product?.product_name;
+        let genericName = res?.product?.generic_name;
+        let ecoScore = res?.product?.ecoscore_grade;
 
-				let dataEcoScore = res?.product?.ecoscore_data;
+        let dataEcoScore = res?.product?.ecoscore_data;
 
-				if (productImageUrl) {
-					this.setState({ productImageUrl: productImageUrl });
-				}
-				if (productName) {
-					this.setState({ productName: productName });
-				}
-				if (genericName) {
-					this.setState({ genericName: genericName });
-				}
-				if (
-					ecoScore &&
-					(ecoScore === "a" ||
-						ecoScore === "b" ||
-						ecoScore === "c" ||
-						ecoScore === "d" ||
-						ecoScore === "e")
-				) {
-					this.setState({ ecoScore: ecoScore });
-				}
-				if (dataEcoScore) {
-					this.setState({ dataEcoScore: dataEcoScore });
-				}
-			});
-	};
+        if (productImageUrl) {
+          this.setState({ productImageUrl: productImageUrl });
+        }
+        if (productName) {
+          this.setState({ productName: productName });
+        }
+        if (genericName) {
+          this.setState({ genericName: genericName });
+        }
+        if (
+          ecoScore &&
+          (ecoScore === "a" ||
+            ecoScore === "b" ||
+            ecoScore === "c" ||
+            ecoScore === "d" ||
+            ecoScore === "e")
+        ) {
+          this.setState({ ecoScore: ecoScore });
+        }
+        if (dataEcoScore) {
+          this.setState({ dataEcoScore: dataEcoScore });
+        }
+      });
+  };
 
-	flip = (event) => {
-		if (event.target.style.transform === "rotateY(360deg)") {
-			event.target.style.transform = "rotateY(0deg)";
-		} else {
-			event.target.style.transform = "rotateY(360deg)";
-		}
-	};
+  flip = (event) => {
+    if (event.target.style.transform === "rotateY(360deg)") {
+      event.target.style.transform = "rotateY(0deg)";
+    } else {
+      event.target.style.transform = "rotateY(360deg)";
+    }
+  };
 
-	displayImage = () => {
-		this.handleBarCodeUpdate();
+  displayImage = () => {
+    this.handleBarCodeUpdate();
 
-		let image = <React.Fragment />;
-		let productName = <React.Fragment />;
-		let genericName = <React.Fragment />;
-		let ecoScore = <React.Fragment />;
-		if (this.state.productImageUrl) {
-			image = (
-				<img
-					src={this.state.productImageUrl}
-					className="product-image"
-					alt="produit"
-				/>
-			);
-		}
+    let image = <React.Fragment />;
+    let productName = <React.Fragment />;
+    let genericName = <React.Fragment />;
+    let ecoScore = <React.Fragment />;
+    if (this.state.productImageUrl) {
+      image = (
+        <img
+          src={this.state.productImageUrl}
+          className="product-image"
+          alt="produit"
+        />
+      );
+    }
 
-		if (this.state.productName) {
-			productName = (
-				<div className="product-name">{this.state.productName}</div>
-			);
-		}
-		if (this.state.ecoScore) {
-			let scoreClass = "color_score_" + this.state.ecoScore;
-			ecoScore = (
-				<div className="product-ecoscore">
-					<span className={"circle-score " + scoreClass}>⬤ </span>
-					EcoScore :
-					<span className="uppercase ">
-						{" " + this.state.ecoScore}
-					</span>
-				</div>
-			);
-		} else if (this.state.genericName) {
-			genericName = (
-				<div className="product-generic-name">
-					{this.state.genericName}
-				</div>
-			);
-		}
-		return (
-			<React.Fragment>
-				{image}
-				{productName}
-				<div className="product-bottom-image-div">
-					{genericName}
-					{ecoScore}
-				</div>
-			</React.Fragment>
-		);
-	};
+    if (this.state.productName) {
+      productName = (
+        <div className="product-name">{this.state.productName}</div>
+      );
+    }
+    if (this.state.ecoScore) {
+      let scoreClass = "color_score_" + this.state.ecoScore;
+      ecoScore = (
+        <div className="product-ecoscore">
+          <span className={"circle-score " + scoreClass}>⬤ </span>
+          EcoScore :
+          <span className="uppercase ">{" " + this.state.ecoScore}</span>
+        </div>
+      );
+    } else if (this.state.genericName) {
+      genericName = (
+        <div className="product-generic-name">{this.state.genericName}</div>
+      );
+    }
+    return (
+      <React.Fragment>
+        {image}
+        {productName}
+        <div className="product-bottom-image-div">
+          {genericName}
+          {ecoScore}
+        </div>
+      </React.Fragment>
+    );
+  };
 
-	handleChange = (event, newValue) => {
-		this.setState({ value: newValue });
-	};
+  handleChange = (event, newValue) => {
+    this.setState({ value: newValue });
+  };
 
-	render = () => {
-		return (
-			<React.Fragment>
-				<div className="product-page-container">
-					<div className="product-header-container">
-						<div className="product-history-link">
-							<Link to="/history"> {"<"} Historique</Link>
-						</div>
-						<img
-							className="product-bitmap-image"
-							src="/images/utils/bitmap.png"
-							alt=""
-						/>
-					</div>
-					<div
-						className="product-image-container"
-						onClick={this.flip}
-					>
-						{this.displayImage()}
-					</div>
+  render = () => {
+    return (
+      <React.Fragment>
+        <div className="product-page-container">
+          <img
+            className="product-bitmap-image"
+            src="/images/utils/bitmap.png"
+            alt=""
+          />
+          <div className="product-header-container">
+            <div className="product-history-link">
+              <Link to="/history"> {"<"} Historique</Link>
+            </div>
+          </div>
+          <div className="product-image-container" onClick={this.flip}>
+            {this.displayImage()}
+          </div>
 
-					<div className="product-navbar-container">
-						<button
-							className={
-								this.state.value === 0
-									? "product-navbar-button selected"
-									: "product-navbar-button"
-							}
-							onClick={() => this.handleChange("", 0)}
-						>
-							<span class="material-icons">travel_explore</span>
-							Traçabilité
-						</button>
-						<button
-							className={
-								this.state.value === 1
-									? "product-navbar-button selected"
-									: "product-navbar-button"
-							}
-							onClick={() => this.handleChange("", 1)}
-						>
-							<span class="material-icons">nature_people</span>
-							Environnement
-						</button>
-					</div>
+          <div className="product-navbar-container">
+            <button
+              className={
+                this.state.value === 0
+                  ? "product-navbar-button selected"
+                  : "product-navbar-button"
+              }
+              onClick={() => this.handleChange("", 0)}
+            >
+              <span class="material-icons">travel_explore</span>
+              Traçabilité
+            </button>
+            <button
+              className={
+                this.state.value === 1
+                  ? "product-navbar-button selected"
+                  : "product-navbar-button"
+              }
+              onClick={() => this.handleChange("", 1)}
+            >
+              <span class="material-icons">nature_people</span>
+              Environnement
+            </button>
+          </div>
 
-					{/* <AppBar position="static">
+          {/* <AppBar position="static">
 						<Tabs
 							value={this.state.value}
 							onChange={this.handleChange}
@@ -259,23 +251,23 @@ class Product extends React.Component {
 					<TabPanel value={this.state.value} index={0}></TabPanel>
 					<TabPanel value={this.state.value} index={1}>
 				</TabPanel> */}
-					{this.state.value === 1 ? (
-						<Environnement
-							dataEcoScore={this.state.dataEcoScore}
-						></Environnement>
-					) : (
-						<div className="product-bottom-container">
-							<Traceability products={this.state.products} />
-						</div>
-					)}
-					<Navbar
-						barcode={this.props.match.params.barcode}
-						bcProductId={this.props.match.params.bcProductId}
-					/>
-				</div>
-			</React.Fragment>
-		);
-	};
+          {this.state.value === 1 ? (
+            <Environnement
+              dataEcoScore={this.state.dataEcoScore}
+            ></Environnement>
+          ) : (
+            <div className="product-bottom-container">
+              <Traceability products={this.state.products} />
+            </div>
+          )}
+          <Navbar
+            barcode={this.props.match.params.barcode}
+            bcProductId={this.props.match.params.bcProductId}
+          />
+        </div>
+      </React.Fragment>
+    );
+  };
 }
 
 export default Product;
