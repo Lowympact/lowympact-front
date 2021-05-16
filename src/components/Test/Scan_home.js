efc0374e87c50b301ceb30611813102d0c994348
corentinbranchereau <branchereaucorentin@gmail.com>
2021-05-16 16:42:04 +0200

    feat: switch between barcode & qrcode

--- src/components/Test/Scan_home.js
+++ src/components/Test/Scan_home.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import Scanner from "./Scanner";
+import QrReader from "react-qr-reader";
+import Switch from "react-switch";
 
 class Scan_home extends Component {
     state = {
-        scanning: false,
+        scanning: true,
         status: "",
         results: [],
         usedCamera: 0,
         devices: [],
+        reading: 0,
+        QRresult: undefined,
     };
 
     componentDidMount = async () => {
@@ -32,7 +36,8 @@ class Scan_home extends Component {
             num = 0;
         }
 
-        this.setState({ usedCamera: num });
+        this.setState({ usedCamera: num, scanning: false });
+        this.setState({ scanning: true });
     };
 
     _scan = () => {
@@ -60,30 +65,104 @@ class Scan_home extends Component {
         );
     };
 
+    handleScan = (data) => {
+        if (data) {
+            this.setState({
+                QRresult: data,
+            });
+        }
+    };
+    handleError = (err) => {
+        console.error(err);
+    };
+
+    displayQrCode = () => {
+        return (
+            <QrReader
+                delay={300}
+                onError={this.handleError}
+                onScan={this.handleScan}
+                style={{ width: "100%" }}
+            />
+        );
+    };
+
+    displayBarCode = () => {
+        if (this.state.devices.length > 0) {
+            return (
+                <React.Fragment>
+                    <div className="header">
+                        {/* <div>Status: {this.state.status}</div>
+                        <div onClick={this._scan}>{this.state.scanning ? "Stop" : "Start"}</div> */}
+                        {this.state.devices?.length > 1 ? (
+                            <button className="code-switch-camera" onClick={this.switchCamera}>
+                                <span className="material-icons">cameraswitch</span>
+                                {this.state.usedCamera}
+                            </button>
+                        ) : (
+                            <React.Fragment />
+                        )}
+                        <ul className="results">
+                            {this.state.results.map((result, i) => (
+                                <div key={result.codeResult.code + i}>
+                                    {result?.codeResult?.code} [{result?.codeResult?.format}]
+                                </div>
+                            ))}
+                        </ul>
+                    </div>
+                    {this.state.scanning ? (
+                        <Scanner
+                            onDetected={this._onDetected}
+                            deviceId={this.state.devices[this.state.usedCamera].deviceId}
+                        />
+                    ) : null}
+                </React.Fragment>
+            );
+        } else {
+            return <React.Fragment />;
+        }
+    };
+
+    switchReader = () => {
+        if (this.state.reading) this.setState({ reading: false });
+        else this.setState({ reading: true });
+    };
+
     render() {
         console.log("Results: ", this.state.results);
         return (
-            <div>
-                <div className="header">
-                    <div>Status: {this.state.status}</div>
-                    <div onClick={this._scan}>{this.state.scanning ? "Stop" : "Start"}</div>
-                    <button onClick={this.switchCamera}>
-                        Current Camera : {this.state.usedCamera}
-                    </button>
-                    <ul className="results">
-                        {this.state.results.map((result, i) => (
-                            <div key={result.codeResult.code + i}>
-                                {result?.codeResult?.code} [{result?.codeResult?.format}]
-                            </div>
-                        ))}
-                    </ul>
-                </div>
-                {this.state.scanning ? (
-                    <Scanner
-                        onDetected={this._onDetected}
-                        deviceId={this.state.devices[this.state.usedCamera].deviceId}
+            <div className="code-reader-container">
+                <div className="code-switch" onClick={this.switchReader}>
+                    <div className="span-code-switch">
+                        <span
+                            className={
+                                this.state.reading ? "material-icons" : "material-icons green"
+                            }
+                        >
+                            view_week
+                        </span>
+                        <span>Barcode</span>
+                    </div>
+                    <Switch
+                        onChange={this.switchReader}
+                        uncheckedIcon={false}
+                        checkedIcon={false}
+                        checked={this.state.reading}
+                        onColor={"#888"}
+                        offColor={"#888"}
                     />
-                ) : null}
+                    <div className="span-code-switch">
+                        <span
+                            className={
+                                !this.state.reading ? "material-icons" : "material-icons green"
+                            }
+                        >
+                            qr_code_scanner
+                        </span>
+                        <span>QR Code</span>
+                    </div>
+                </div>
+                {this.state.reading ? this.displayQrCode() : this.displayBarCode()}
             </div>
         );
     }
