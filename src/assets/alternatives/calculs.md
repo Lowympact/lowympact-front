5d5b7c8fb2a78bafbdce3c2ce2d0a1e5af8d4c83
corentinbranchereau <branchereaucorentin@gmail.com>
2021-05-26 16:34:00 +0200

    feat: new alternatives

---
+++ src/assets/alternatives/calculs.md
@@ -0,0 +1,53 @@
+# Calculs des alternatives
+
+Toutes les données proviennent du site de l'[ADEME]
+
+[ademe]: https://www.bilans-ges.ademe.fr/fr/basecarbone/donnees-consulter/
+
+## Electricité
+
+Bilan carbone de la production éléctrique moyenne en France : 0.06 kgCO2e/kWh (+-0.005 kgCO2e/Kwh)
+
+## 500 feuilles de papier blanc 80g
+
+2290 gCO2e/unité --> 1 feuille = 4.6 gCO2e
+
+## Pomme de terre fr
+
+0.6 kgCO2e/kg de poids net
+
+## Fraise fr
+
+0.525 kgCO2e/kg de poids net
+
+## Voiture
+
+0.193 kgCO2e/km
+
+## Bus
+
+0.137 kgCO2e/passager.km
+
+## envoi d'un Email (pièce jointe de 1mo)
+
+19g CO2e /email ([source](https://www.orange.be/fr/blog/reduire-empreinte-carbone-mail))
+
+## Ampoule incandescence
+
+0.5 kgCO2e/jour
+
+## Ampoule basse Consommation
+
+0.083 kgCO2e/jour
+
+## chauffage au gaz d’un appartement de 70 m2
+
+0.24 kgCO2e/heure
+
+## Steak haché de boeuf cru
+
+33.8 kgCO2e/kg de poids net
+
+## Trotinette électrique
+
+0.105 kgCO2e/kilomètre par passager,
--- src/components/Environnement/EmpreinteCarbone.js
+++ src/components/Environnement/EmpreinteCarbone.js
@@ -1,142 +1,243 @@
 import React from "react";
-import trainImage from "../../assets/images/train.png";
 
 class EmpreinteCarbone extends React.Component {
     state = {
-        visible: false,
+        alea: 3,
     };
 
-    setVisible = () => {
-        this.setState({ visible: !this.state.visible });
+    setAlea = () => {
+        // this.setState({ alea: Math.floor(Math.random() * 11) });
+        let alea = this.state.alea + 1;
+        if (alea > 11) alea = 0;
+        this.setState({ alea: alea });
     };
 
     render = () => {
-        console.log(this.props.dataEcoScore);
-        let repartition = [
-            {
-                image: "agriculture",
-                name: "Agriculture",
-                percent: (
-                    (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_agriculture) * 100) /
-                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                ).toFixed(0),
-            },
-            {
-                image: "local_shipping",
-                name: "Transport",
-                percent: (
-                    (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_transportation) * 100) /
-                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                ).toFixed(0),
-            },
-            {
-                image: "view_in_ar",
-                name: "Emballage",
-                percent: (
-                    (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_packaging) * 100) /
-                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                ).toFixed(0),
-            },
-            {
-                image: "transform",
-                name: "Transformation",
-                percent: (
-                    (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_processing) * 100) /
-                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                ).toFixed(0),
-            },
-            {
-                image: "store",
-                name: "Distribution",
-                percent: (
-                    (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_distribution) * 100) /
-                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                ).toFixed(0),
-            },
-            {
-                image: "microwave",
-                name: "Consommation",
-                percent: (
-                    (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_consumption) * 100) /
-                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                ).toFixed(0),
-            },
-        ];
-        repartition = repartition.map((i) => {
-            return (
-                <div className="empreinte-single">
-                    <div className="material-icons">{i.image}</div>
-                    <div className="name">{i.name} : </div>
-                    <div className="pourcent">{i.percent} %</div>
-                </div>
+        if (this.props.dataEcoScore?.agribalyse?.co2_total) {
+            console.log(this.props.dataEcoScore);
+            let repartition = [
+                {
+                    image: "agriculture",
+                    name: "Agriculture",
+                    percent: (
+                        (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_agriculture) * 100) /
+                        parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
+                    ).toFixed(0),
+                },
+                {
+                    image: "local_shipping",
+                    name: "Transport",
+                    percent: (
+                        (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_transportation) *
+                            100) /
+                        parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
+                    ).toFixed(0),
+                },
+                {
+                    image: "view_in_ar",
+                    name: "Emballage",
+                    percent: (
+                        (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_packaging) * 100) /
+                        parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
+                    ).toFixed(0),
+                },
+                {
+                    image: "transform",
+                    name: "Transformation",
+                    percent: (
+                        (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_processing) * 100) /
+                        parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
+                    ).toFixed(0),
+                },
+                {
+                    image: "store",
+                    name: "Distribution",
+                    percent: (
+                        (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_distribution) * 100) /
+                        parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
+                    ).toFixed(0),
+                },
+                {
+                    image: "microwave",
+                    name: "Consommation",
+                    percent: (
+                        (parseFloat(this.props.dataEcoScore?.agribalyse?.co2_consumption) * 100) /
+                        parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
+                    ).toFixed(0),
+                },
+            ];
+            repartition = repartition.map((i) => {
+                return (
+                    <div className="empreinte-single">
+                        <div className="material-icons">{i.image}</div>
+                        <div className="name">{i.name} : </div>
+                        <div className="pourcent">{i.percent} %</div>
+                    </div>
+                );
+            });
+
+            let equivalent = getEquivalent(
+                parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total),
+                this.state.alea
             );
-        });
-        return (
-            <React.Fragment>
-                <div className="empreinte-carbone-container" onClick={this.setVisible}>
-                    <div className="empreinte-carbone-header">
-                        <div className="header-text">
-                            <div className="header-main">Empreinte Carbone</div>
-                            <div className="header-small">Estimée en kg de CO2 équivalent</div>
-                        </div>
-                        <div className="empreinte-carbone-number">
-                            <span>
-                                {parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total).toFixed(
-                                    2
-                                )}
-                                kg
-                            </span>
-                            <div className="circle"></div>
-                        </div>
-                    </div>
-                    <div className="empreinte-carbone-equivalent">
-                        <div className="icon-container">
-                            <div className="material-icons">
-                                <img src={trainImage} />
+
+            let circle = "cmauve";
+            if (this.props.dataEcoScore?.agribalyse?.co2_total < 15) circle = "cred";
+            if (this.props.dataEcoScore?.agribalyse?.co2_total < 7) circle = "corange";
+            if (this.props.dataEcoScore?.agribalyse?.co2_total < 3) circle = "cgreen";
+            if (this.props.dataEcoScore?.agribalyse?.co2_total < 1) circle = "cdarkgreen";
+            return (
+                <React.Fragment>
+                    <div className="empreinte-carbone-container">
+                        <div className="empreinte-carbone-header">
+                            <div className="header-text">
+                                <div className="header-main">Empreinte Carbone</div>
+                                <div className="header-small">source : ADEME</div>
+                            </div>
+                            <div className="empreinte-carbone-number">
+                                <span>
+                                    {parseFloat(
+                                        this.props.dataEcoScore?.agribalyse?.co2_total
+                                    ).toFixed(2)}
+                                    kg CO2e
+                                </span>
+                                <div className={"circle " + circle}></div>
                             </div>
                         </div>
-                        <div className="equivalent-container">
-                            <div className="text-small">Soit l'équivalent...</div>
-                            <div className="text-big">
-                                {getEquivalent(
-                                    parseFloat(this.props.dataEcoScore?.agribalyse?.co2_total)
-                                )}
+                        <div className="empreinte-carbone-equivalent">
+                            <div className="icon-container">
+                                <div className="material-icons">
+                                    <img src={`/images/alternatives/${equivalent[1]}`} />
+                                </div>
+                            </div>
+                            <div className="equivalent-container">
+                                <div className="text-small">Soit l'équivalent...</div>
+                                <div className="text-big">{equivalent[0]}</div>
                             </div>
                         </div>
-                    </div>
-                    <div className="empreinte-carbone-repartition">
-                        <div
-                            className={
-                                this.state.visible ? " animation-appear" : "animation-disappear"
-                            }
-                        >
-                            {repartition}
+                        <div className="arrow" onClick={this.setAlea}>
+                            <div className="material-icons">refresh</div>
                         </div>
                     </div>
-                    <div className="arrow">
-                        <div className="material-icons">
-                            {this.state.visible ? "arrow_drop_up" : "arrow_drop_down"}
+
+                    <div className="empreinte-carbone-container">
+                        <div className="empreinte-carbone-header">
+                            <div className="header-text">
+                                <div className="header-main">
+                                    Répartition de l'empreinte Carbone
+                                </div>
+                                <div className="header-small">
+                                    selon les différentes étapes du cycle de vie
+                                </div>
+                            </div>
+                            {/* <div className="empreinte-carbone-number">
+                                <span>
+                                    {parseFloat(
+                                        this.props.dataEcoScore?.agribalyse?.co2_total
+                                    ).toFixed(2)}
+                                    kg CO2e
+                                </span>
+                                <div className="circle"></div>
+                            </div> */}
                         </div>
+                        <div className="empreinte-carbone-repartition">{repartition}</div>
                     </div>
-                </div>
-            </React.Fragment>
-        );
+                </React.Fragment>
+            );
+        } else {
+            return <React.Fragment />;
+        }
     };
 }
 
 export default EmpreinteCarbone;
 
-function getEquivalent(co2) {
-    if (co2 < 0.5) return "D'un aller Paris-Angers en TGV (300km)";
-    if (co2 >= 0.5 && co2 < 1) return "D'un aller Paris-Lyon en TGV (420km)";
-    if (co2 >= 1 && co2 < 1.5) return "D'un aller Lyon-Nantes en TGV (800km)";
-    if (co2 >= 1.5 && co2 < 2) return "D'un aller Lille-Marseille en TGV (960km)";
-    if (co2 >= 2 && co2 < 2.5) return "D'un aller Nantes-Nice en TGV (1350km)";
-    if (co2 >= 2.5 && co2 < 3) return "D'un aller-retour Lyon-Nantes en TGV (1600km)";
-    if (co2 >= 3 && co2 < 4) return "D'un aller-retour Paris-Nice en TGV (2000km)";
-    if (co2 >= 4 && co2 < 5) return "De 3 allers Paris-Nice en TGV (3000km)";
-    if (co2 >= 5 && co2 < 6) return "De 2 aller-retours Paris-Nice en TGV (4000km)";
-    if (co2 >= 6 && co2 < 8) return "De 3 aller-retours Paris-Nice en TGV (6000km)";
-    if (co2 >= 8 && co2 < 12) return "De 4 aller-retours Paris-Nice en TGV (8000km)";
-    if (co2 >= 12) return "De plus de 5 aller-retours Paris-Nice en TGV (>10 000km)";
+function getEquivalent(co2, alea) {
+    switch (alea) {
+        case 0:
+            return [
+                "De la production de " +
+                    (co2 / 0.0046).toFixed(0) +
+                    " feuilles de papier blanc 80g",
+                "paper.png",
+            ];
+        case 1:
+            return [
+                "De la production de " +
+                    (co2 / 0.6).toFixed(1) +
+                    " kg de pomme de terres en France",
+                "potatoe.png",
+            ];
+        case 2:
+            return [
+                "De la production de " + (co2 / 0.53).toFixed(1) + " kg de fraises en France",
+                "strawberry.png",
+            ];
+        case 3:
+            return [
+                "De " + (co2 / 0.193).toFixed(1) + " km parcourus par une personne en voiture",
+                "car.svg",
+            ];
+        case 4:
+            return [
+                "De " + (co2 / 0.137).toFixed(1) + " km parcourus par une personne en Bus de ville",
+                "bus.svg",
+            ];
+        case 5:
+            return [
+                "De " +
+                    (co2 / 0.019).toFixed(0) +
+                    " e-mails envoyés contenant une pièce jointe d'1mo",
+                "mail.svg",
+            ];
+        case 6:
+            return [
+                "De " +
+                    (co2 / 0.5).toFixed(1) +
+                    " jour(s) d'éclairage d'une ampoule à incandescence",
+                "light.svg",
+            ];
+        case 7:
+            return [
+                "De " +
+                    (co2 / 0.083).toFixed(1) +
+                    " jours d'éclairage d'une ampoule basse consommation",
+                "light.svg",
+            ];
+        case 8:
+            return [
+                "De " +
+                    (co2 / 0.24).toFixed(1) +
+                    " heures de chauffage au gaz d'un appartement moyen de 70m²",
+                "appartment.svg",
+            ];
+        case 9:
+            return [
+                "De la production de " +
+                    ((co2 * 100) / 33.8).toFixed(0) +
+                    " grammes de steak haché de boeuf cru",
+                "steak.png",
+            ];
+        case 10:
+            return [
+                "De " +
+                    (co2 / 0.105).toFixed(1) +
+                    " kilomètres parcourus en Trotinette électrique (en France)",
+                "trotinette.png",
+            ];
+        case 11:
+            return [
+                "D'une personne parcourant " +
+                    (co2 / 0.3).toFixed(0) * 100 +
+                    " kilomètres en TGV (en France)",
+                "train.png",
+            ];
+        default:
+            return [
+                "De " +
+                    (co2 / 0.105).toFixed(1) +
+                    " kilomètres parcourus en Trotinette électrique (en France)",
+                "trotinette.png",
+            ];
+    }
 }
