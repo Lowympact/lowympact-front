69aed598ca0507224e3d29f5c30ce8c1213f3fac
corentinbranchereau <branchereaucorentin@gmail.com>
2021-05-29 01:03:58 +0200

    feat: cache large files

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,25 +1,77 @@
-let CACHE_NAME = 'lowympact-cache-v1';
-const urlsToCache = [
-'/',
-'/index.html',
+let CACHE_NAME = "lowympact-cache-v1.0.1";
+const CACHED_URLS = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/images/alternatives/appartement.svg",
+    "/images/alternatives/bus.svg",
+    "/images/alternatives/car.svg",
+    "/images/alternatives/light.svg",
+    "/images/alternatives/mail.svg",
+    "/images/alternatives/paper.png",
+    "/images/alternatives/potatoe.png",
+    "/images/alternatives/steak.png",
+    "/images/alternatives/strawberry.png",
+    "/images/alternatives/train.png",
+    "/images/alternatives/trotinette.png",
+    "/images/utils/bitmap.png",
+    "/images/utils/ecnoScoreA.png",
+    "/images/utils/ecnoScoreB.png",
+    "/images/utils/ecnoScoreC.png",
+    "/images/utils/ecnoScoreD.png",
+    "/images/utils/ecnoScoreE.png",
+    "/images/utils/ecnoScoreFull.png",
+    "/images/utils/loading.gif",
+    "/images/utils/logo.png",
+    "/images/utils/logo.svg",
+    "/images/utils/map.png",
+    "/images/utils/map2.png",
+    "/images/utils/map3.png",
+    "/pwa-192x192.png",
+    "/pwa-512x512.png",
 ];
-self.addEventListener('install', function(event) {
-    // Perform install steps
+
+// Open cache on install.
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
+        (async function () {
+            const cache = await caches.open(CACHE_NAME);
+
+            await cache.addAll(CACHED_URLS);
+        })()
     );
-    self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request);
-      })
-    );
-  });
+// Cache and update with stale-while-revalidate policy.
+self.addEventListener("fetch", (event) => {
+    const { request } = event;
 
+    // Prevent Chrome Developer Tools error:
+    // Failed to execute 'fetch' on 'ServiceWorkerGlobalScope': 'only-if-cached' can be set only with 'same-origin' mode
+    //
+    // See also https://stackoverflow.com/a/49719964/1217468
+    // if (request.cache === "only-if-cached" && request.mode !== "same-origin") {
+    //     return;
+    // }
+
+    event.respondWith(
+        (async function () {
+            const cache = await caches.open(CACHE_NAME);
+
+            const cachedResponsePromise = await cache.match(request);
+            const networkResponsePromise = fetch(request);
+
+            if (request.url.startsWith(self.location.origin)) {
+                event.waitUntil(
+                    (async function () {
+                        const networkResponse = await networkResponsePromise;
+
+                        await cache.put(request, networkResponse.clone());
+                    })()
+                );
+            }
+            console.log((cachedResponsePromise || networkResponsePromise) == cachedResponsePromise);
+            return cachedResponsePromise || networkResponsePromise;
+        })()
+    );
+});
