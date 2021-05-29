5c0c08a4c6033940241c205c46f92ac5dedf0b87
corentinbranchereau <branchereaucorentin@gmail.com>
2021-05-29 21:57:00 +0200

    build

--- build/serviceWorker.js
+++ build/serviceWorker.js
@@ -1,4 +1,4 @@
-let CACHE_NAME = "lowympact-cache-v1.0.5";
+let CACHE_NAME = "lowympact-cache-v1.0.6";
 const CACHED_URLS = ["/", "/index.html", "/manifest.json"];
 // "/images/alternatives/appartement.svg",
 // "/images/alternatives/bus.svg",
