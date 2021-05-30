0558b22eaab03d6710649ae6f70e22def0028d68
corentinbranchereau <branchereaucorentin@gmail.com>
2021-05-31 00:47:40 +0200

    build

--- build/static/js/16.dd0d5f82.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplowympact=this.webpackJsonplowympact||[]).push([[16],{317:function(e,c,s){"use strict";s.r(c);var t=s(38),a=s(16),r=s(1);c.default=function(){return Object(r.jsx)("div",{className:"App",children:Object(r.jsxs)("header",{className:"App-header",children:[Object(r.jsx)("img",{src:t.a,className:"logo",alt:"logo"}),Object(r.jsx)("h1",{children:"Erreur 404"}),Object(r.jsx)(a.b,{to:"/",children:"Retourner \xe0 l'accueil"})]})})}},38:function(e,c,s){"use strict";c.a=s.p+"static/media/logo.b235ef42.svg"}}]);
-//# sourceMappingURL=16.dd0d5f82.chunk.js.map
